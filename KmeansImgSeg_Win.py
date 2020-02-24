ebf673a44c9e29f3cd58fdc7066a608c234a4309
dyno0 <dynob@windowslive.com>
2020-02-24 20:05:07 +0100

    v3

---
+++ KmeansImgSeg_Win.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+from sklearn.cluster import KMeans
+
+pic = input("Ime slike:")
+path = "images\\" + pic
+slika = cv2.imread(path)
+h, w = slika.shape[:2]
+
+slika = cv2.cvtColor(slika, cv2.COLOR_BGR2LAB)
+slika = slika.reshape((slika.shape[0] * slika.shape[1], 3))
+
+n_k = int(input("Broj clustera:"))
+k = KMeans(n_clusters = n_k)
+
+segmenti = k.fit_predict(slika)
+seg_slika = k.cluster_centers_.astype("uint8")[segmenti]
+
+seg_slika = seg_slika.reshape((h, w, 3))
+slika = slika.reshape((h, w, 3))
+
+seg_slika = cv2.cvtColor(seg_slika, cv2.COLOR_LAB2BGR)
+slika = cv2.cvtColor(slika, cv2.COLOR_LAB2BGR)
+
+cv2.imshow('segmented.jpg', seg_slika)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
